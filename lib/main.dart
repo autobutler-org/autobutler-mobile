1c960a226aa9b39d9ee90163210f8be106455ff9
James Orson <jamesaorson@gmail.com>
2026-02-21 17:30:14 -0800

    autobutler-skeleton: Initial mock
    Signed-off-by: James Orson <jamesaorson@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,217 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const AutobutlerApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class AutobutlerApp extends StatelessWidget {
+  const AutobutlerApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Autobutler Mobile',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+          brightness: Brightness.dark,
+        ),
+        scaffoldBackgroundColor: const Color(0xFF070D19),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const FileBrowserPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+enum BrowserItemType { folder, image, archive }
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+class FileBrowserItem {
+  const FileBrowserItem({
+    required this.name,
+    required this.device,
+    required this.size,
+    required this.type,
+  });
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  final String name;
+  final String device;
+  final String size;
+  final BrowserItemType type;
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+const mockFileBrowserItems = <FileBrowserItem>[
+  FileBrowserItem(
+    name: 'flipped_(1).jpg',
+    device: 'Data',
+    size: '6.2 MB',
+    type: BrowserItemType.image,
+  ),
+  FileBrowserItem(
+    name: 'flipped_(2).jpg',
+    device: 'Data',
+    size: '6.2 MB',
+    type: BrowserItemType.image,
+  ),
+  FileBrowserItem(
+    name: 'flipped_(3).jpg',
+    device: 'Data',
+    size: '6.2 MB',
+    type: BrowserItemType.image,
+  ),
+  FileBrowserItem(
+    name: 'flipped_(4).jpg',
+    device: 'Data',
+    size: '6.2 MB',
+    type: BrowserItemType.image,
+  ),
+  FileBrowserItem(
+    name: 'flipped_(5).jpg',
+    device: 'Data',
+    size: '6.2 MB',
+    type: BrowserItemType.image,
+  ),
+  FileBrowserItem(
+    name: 'flipped_(6).jpg',
+    device: 'Data',
+    size: '6.2 MB',
+    type: BrowserItemType.image,
+  ),
+  FileBrowserItem(
+    name: 'flipped_(7).jpg',
+    device: 'Data',
+    size: '6.2 MB',
+    type: BrowserItemType.image,
+  ),
+  FileBrowserItem(
+    name: 'flipped.jpg',
+    device: 'Data',
+    size: '6.2 MB',
+    type: BrowserItemType.image,
+  ),
+  FileBrowserItem(
+    name: 'Google_Data_autobutler.org@gmail.com_1769933022.zip',
+    device: 'Data',
+    size: '42.3 KB',
+    type: BrowserItemType.archive,
+  ),
+  FileBrowserItem(
+    name: 'project-assets',
+    device: 'Data',
+    size: '--',
+    type: BrowserItemType.folder,
+  ),
+];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class FileBrowserPage extends StatelessWidget {
+  const FileBrowserPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final colors = Theme.of(context).colorScheme;
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Photos'),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.grid_view_rounded),
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16, 16, 16, 12),
+            child: Row(
+              children: [
+                FilledButton.tonalIcon(
+                  onPressed: () {},
+                  icon: const Icon(Icons.upload_rounded),
+                  label: const Text('Upload'),
+                ),
+                const SizedBox(width: 8),
+                OutlinedButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(Icons.create_new_folder_outlined),
+                  label: const Text('New Folder'),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16, 0, 16, 8),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'cirrus / testfolder',
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+            decoration: BoxDecoration(
+              color: colors.surface,
+              border: Border(
+                top: BorderSide(color: colors.outlineVariant.withOpacity(0.6)),
+                bottom: BorderSide(
+                  color: colors.outlineVariant.withOpacity(0.6),
+                ),
+              ),
+            ),
+            child: const Row(
+              children: [
+                Expanded(flex: 6, child: Text('Name')),
+                Expanded(flex: 2, child: Text('Device')),
+                Expanded(flex: 2, child: Text('Size')),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.separated(
+              itemCount: mockFileBrowserItems.length,
+              separatorBuilder: (_, __) => Divider(
+                height: 1,
+                color: colors.outlineVariant.withOpacity(0.5),
+              ),
+              itemBuilder: (context, index) {
+                final item = mockFileBrowserItems[index];
+                return ListTile(
+                  contentPadding: const EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 2,
+                  ),
+                  leading: Icon(_iconForType(item.type)),
+                  title: Row(
+                    children: [
+                      Expanded(flex: 5, child: Text(item.name)),
+                      Expanded(flex: 2, child: Text(item.device)),
+                      Expanded(flex: 2, child: Text(item.size)),
+                    ],
+                  ),
+                  trailing: const Icon(Icons.more_vert),
+                  onTap: () {},
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+  static IconData _iconForType(BrowserItemType type) {
+    switch (type) {
+      case BrowserItemType.folder:
+        return Icons.folder_outlined;
+      case BrowserItemType.image:
+        return Icons.image_outlined;
+      case BrowserItemType.archive:
+        return Icons.archive_outlined;
+    }
   }
 }
